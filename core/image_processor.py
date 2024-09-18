d01f54aac28d6876d74762c2be32db3d78baf879
Fylnex <gojorvover665@gmail.com>
2024-09-18 17:07:54 +0300

    .

--- core/image_processor.py
+++ core/image_processor.py
@@ -27,15 +27,24 @@ class ImageProcessor:
             self.logger.error(f"Error loading image: {str(e)}")
             raise e
 
-    def save_image(self, image, output_path):
+    def save_image(self, image, output_path, format=None):
         """
         Сохранение изображения по указанному пути.
 
         :param image: Image object, изображение для сохранения
         :param output_path: str, путь для сохранения
+        :param format: str, формат для сохранения (если не указано в пути)
         """
         try:
-            image.save(output_path)
+            # Проверяем, указано ли расширение в пути
+            if not format:
+                if "." in output_path:
+                    format = output_path.split(".")[-1].upper()  # Получаем формат из расширения файла
+                else:
+                    format = "PNG"  # Если расширения нет, используем PNG по умолчанию
+                    output_path += ".png"  # Добавляем расширение .png, если его нет
+
+            image.save(output_path, format=format)
             self.logger.info(f"Image saved successfully at {output_path}")
         except Exception as e:
             self.logger.error(f"Error saving image: {str(e)}")
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 # from gui import GUI
 from core import ImageProcessor, Encryption, Embedder, Extractor
+import os
 
 if __name__ == "__main__":
     # Инициализация классов
@@ -9,10 +10,10 @@ if __name__ == "__main__":
     extractor = Extractor()
 
     # Путь к изображению и данным
-    image_path = "tests/image/pngwing.com.png"
-    data_path = "tests/text/exam.txt"
-    output_image_path = "output_image.png"
-    password = "securepassword"
+    image_path = "tests/image/pngwing.com.png" # Путь к изображению
+    data_path = "tests/text/exam.txt" # Путь к текстовому файла
+    output_image_path = os.path.join(os.path.expanduser('~'), 'Downloads', 'modified_image.png') # Путь к выходному изображению
+    password = "securepassword" # Пароль для шифрования и дешифрования
 
     # Чтение данных из файла
     with open(data_path, "rb") as file:
@@ -28,7 +29,9 @@ if __name__ == "__main__":
     # Извлечение данных из изображения
     extracted_data = extractor.extract_data(modified_image, password, encryption)
 
+
+
     # Сохранение извлеченных данных
-    with open("extracted_data.txt", "wb") as file:
+    with open(os.path.join(os.path.expanduser('~'), 'Downloads', 'extracted_data.txt'), encoding="utf-8") as file:
         file.write(extracted_data)
 
